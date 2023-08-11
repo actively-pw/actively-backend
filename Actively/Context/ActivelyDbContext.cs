e522c0c590c296c1b4b5bf5b0f416bc8741777c3
LauraKorona <104787413+LauraKorona@users.noreply.github.com>
2023-08-11 17:14:50 +0200

    Activity model (#2)
    * Add Activity model and db context
    
    * Add local database

---
+++ Actively/Context/ActivelyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Actively.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Actively.Context
+{
+	public class ActivelyDbContext : DbContext
+	{
+		public ActivelyDbContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		public DbSet<Activity> Activities { get; set; }
+	}
+}
--- Actively/Program.cs
+++ Actively/Program.cs
@@ -1,3 +1,6 @@
+using Actively.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ActivelyDbContext>(options =>
+		options.UseSqlServer(builder.Configuration["ConnectionStrings:DbConnectionString"]));
 
 var app = builder.Build();
 
