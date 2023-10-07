94f76cd1f55936e3959bee7322eef6616f2010a1
LauraKorona <104787413+LauraKorona@users.noreply.github.com>
2023-10-07 18:50:17 +0200

    Authorization (#12)
    * Add bearer authorization
    
    * Fix sorting in GetAllActivities

--- Actively/Controllers/ActivityController.cs
+++ Actively/Controllers/ActivityController.cs
@@ -3,6 +3,7 @@ using Actively.Controllers.Repositories.Interfaces;
 using Actively.Models;
 using Actively.Models.DTOs;
 using Actively.Services.GeoJsonGenerator.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
@@ -24,13 +25,14 @@ namespace Actively.Controllers
 		}
 
 		[HttpGet]
+		[Authorize]
 		public async Task<ActionResult<List<GetActivityDto>>> GetAllActivities([FromQuery] PaginationParams @params)
 		{
 			try
 			{
 				var activities = await _activityRepository.GetAllActivities();
 				var enumerable = activities.ToList();
-				enumerable.Sort((a, b) => a.Start.CompareTo(b.Start));
+				enumerable.Sort((a, b) => b.Start.CompareTo(a.Start));
 
 				if (!enumerable.Any()) return NotFound();
 
@@ -54,6 +56,7 @@ namespace Actively.Controllers
 		}
 
 		[HttpPost]
+		[Authorize]
 		public async Task<ActionResult<Activity>> AddActivity(AddActivityDto addActivityDto)
 		{
 			try
@@ -79,6 +82,7 @@ namespace Actively.Controllers
 		}
 
 		[HttpDelete("{id}")]
+		[Authorize]
 		public async Task<ActionResult<Activity>> DeleteActivity(Guid id)
 		{
 			try
@@ -99,6 +103,7 @@ namespace Actively.Controllers
 			}
 		}
 		[HttpPatch("{id}")]
+		[Authorize]
 		public async Task<ActionResult<Activity>> EditActivity(Guid id, [FromBody] JsonPatchDocument<Activity> patchDoc)
 		{
 			try
--- Actively/Program.cs
+++ Actively/Program.cs
@@ -15,6 +15,7 @@ using Azure.Identity;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -25,13 +26,46 @@ builder.Services.AddControllers(options =>
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 
 builder.Configuration.AddAzureKeyVault(
 	new Uri(builder.Configuration.GetSection("KeyVaultUrl").Value!),
 	new DefaultAzureCredential()
 	);
+
+builder.Services.AddSwaggerGen(c =>
+{
+	c.SwaggerDoc("v1", new OpenApiInfo { Title = "Actively" });
+
+	c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+	{
+		Name = "Authorization",
+		Description = "This API uses bearer authentication. Bearer token must be passed as \"Bearer PasteJwtHere\".",
+		In = ParameterLocation.Header,
+		Type = SecuritySchemeType.ApiKey,
+		Scheme = "Bearer"
+	});
+
+	c.AddSecurityRequirement(new OpenApiSecurityRequirement
+	{
+		{
+		new OpenApiSecurityScheme
+		{
+			Name="Bearer",
+			Scheme="oauth2",
+			In = ParameterLocation.Header,
+			Reference = new OpenApiReference
+			{
+				Type = ReferenceType.SecurityScheme,
+				Id = "Bearer"
+			}
+		},
+		new List<string>()
+		}
+	});
+
+}
+);
 
 builder.Services
 	.AddDbContext<ActivelyDbContext>(options =>
